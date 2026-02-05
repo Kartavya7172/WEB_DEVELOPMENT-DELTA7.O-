ffd7d2e67facef21767d8a0de66247af7e7ef234
Kartavya7172 <Kartavyatonpe7172@gmail.com>
2026-02-05 22:58:48 +0530

    new updated

--- Backend2_node_express/index.js
+++ Backend2_node_express/index.js
@@ -9,3 +9,18 @@ let port = 8080;
 app.listen(port,()=>{
   console.log(`app is listening on port ${port}`);
 });
+
+app.use((req,res)=>{
+  console.log("request recevied");
+//   res.send("This is the basic response");
+
+  //sending the object in response
+//   res.send({
+//     name:"apple",
+//     color:"balck",
+//   })
+
+  //sending the html page
+  let code = "<h1>Fruits</h1><ul><li>Orange</li><li>Mango</li></ul>"
+  res.send(code);
+});
